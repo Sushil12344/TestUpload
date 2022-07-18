f59dd71fb421040cfc4012a46568cf539c38ad53
Sushil <sushil.kumar10@ibm.com>
2022-07-18 09:13:02 +0530

    Initial Commit

---
+++ Shared/TestUploadApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestUploadApp.swift
+//  Shared
+//
+//  Created by Sushil Kumar on 18/07/22.
+//
+
+import SwiftUI
+
+@main
+struct TestUploadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
